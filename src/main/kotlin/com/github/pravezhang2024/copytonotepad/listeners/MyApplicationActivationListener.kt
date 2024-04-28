85d67959e9f097925e01908da689fec7440fc93a
GitHub Action <action@github.com>
2024-04-28 08:10:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/pravezhang2024/copytonotepad/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/pravezhang2024/copytonotepad/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pravezhang2024.copytonotepad.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/pravezhang2024/copytonotepad/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pravezhang2024/copytonotepad/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.pravezhang2024.copytonotepad.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pravezhang2024.copytonotepad.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
